34aa103c21cd6046e7144a5b4ad041d89eee7237
Anthony DiRusso <ossurid@gmail.com>
2019-04-10 14:56:20 -0400

    adds random button

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -64,21 +64,44 @@ async function simulatePurchase(req, res) {
  // 1. needs to deduct x amount of stock from whatever item was just purchased
   let i = 0;
   console.log(req.body);
+  
   const itemName = req.body.itemSelect.replace(/-/g, ' ');
-  const itemInstance = await Item.getByName(itemName);
-  while (i < req.body.customerCount) {
-    const itemID = itemInstance.id;
-
-    await Item.adjustStock(-1, itemID);
-
-    const date = '2019-04-10';
-
+  if (req.body.itemSelect === "random") {
+    //find length of allItmes array - getAllItems
+    const allItems = await Item.getAll();
+    const numberOfItems = allItems.length;
+    while (i < req.body.customerCount) {
+      const randomItem = allItems[Math.floor(numberOfItems * Math.random())];
+      const randomItemName = randomItem.name;
+      const itemInstance = await Item.getByName(randomItemName);
+      const itemID = itemInstance.id;
   
-  // 2. needs to create a record of the purchase in purchases table
+      await Item.adjustStock(-1, itemID);
   
-  await Purchase.newPurchase(itemID, 2, 1, date);
-  i++;
+      const date = '2019-04-10';
+      await Purchase.newPurchase(itemID, 2, 1, date);
+      i++;
+    }
+    //run while loop replacing itemid with random less than allItemsArray.length
+
+  } else  {
+    //specific/normal operation 
+    const itemInstance = await Item.getByName(itemName);
+    while (i < req.body.customerCount) {
+      const itemID = itemInstance.id;
+  
+      await Item.adjustStock(-1, itemID);
+  
+      const date = '2019-04-10';
+  
+    
+    // 2. needs to create a record of the purchase in purchases table
+    
+    await Purchase.newPurchase(itemID, 2, 1, date);
+    i++;
+    }
   }
+ 
   // retrieve sum of revenues for the day
   let sum = await Purchase.totalRevenue()
   req.session.sum = sum;
