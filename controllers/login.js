a278d3649f1fec60985f4598b2fb7f481774a4fd
Anthony DiRusso <ossurid@gmail.com>
2019-04-12 10:38:13 -0400

    adds working session save and login

--- controllers/login.js
+++ controllers/login.js
@@ -14,22 +14,26 @@ async function verifyUser(req, res) {
     //set session email
     console.log('req.body.password login.js/9:', req.body.password);
     req.session.email = req.body.email;
-    req.session.username = req.body.username;
+    // req.session.username = req.body.username;
 
-    req.session.save( async () => { 
-        //get the email from the post body
-        const theUser = await User.getByEmail(`${req.body.email}`);
+    //get the email from the post body
+    const theUser = await User.getByEmail(`${req.body.email}`);
+    console.log('this is theUser:', theUser);
+    // res.send('this is working');
 
-        //if the user not found, redirect to the signup page
-        if (theUser === null) {
-            res.redirect('/signup');
-        } else {
+    //if the user not found, redirect to the signup page
+    if (theUser.password) {
+        req.session.save(() => {
             res.redirect('/');
+        })
+        
+    } else {
+        res.redirect('/signup');
 
-        }
-    });    
+    }
+        
 }
 
-module.exports =  {showLoginPage, verifyUser} ;
+module.exports =  {showLoginPage, verifyUser};
 
 
